666ca0de1d85099568d80bc926a59378d735b8e2
Sabina <citrtoj@gmail.com>
2024-05-13 21:38:17 +0300

    add signs scraper

--- scraper/index.js
+++ scraper/index.js
@@ -1,8 +1,9 @@
 import { scrapeQuestions } from "./questions.js";
+import { scrapeSigns } from "./signs.js";
 import { scrapeTheory } from "./theory.js";
 
 // TODO: potentially one single thread for each type of operation
 
 (async () => {
-  scrapeTheory();
+  scrapeSigns();
 })();
---
+++ scraper/signs.js
@@ -0,0 +1,107 @@
+import { DOMParser } from "xmldom";
+import { getContent, silencedDOMParserOptions } from "./utils.js";
+import xpath from "xpath";
+
+// each module has its own DOM parser
+const silencedDOMParser = new DOMParser(silencedDOMParserOptions);
+
+const getSignCategoryLinks = (content) => {
+  let doc = silencedDOMParser.parseFromString(content);
+  const nodes = xpath.select(
+    "//*[contains(@class, 'semne-de-circulatie')]//*[contains(@class, 'card-link')]/@href",
+    // "//*",
+    doc
+  );
+
+  const links = [...nodes.map((node) => node.nodeValue)];
+  return links;
+};
+
+const processLinks = async (links) => {
+  const processedData = await Promise.all(
+    links.map(async (link, index) => {
+      await new Promise((resolve) => setTimeout(resolve, 500 * index));
+      const linkParts = link.split("/");
+      const categoryId = linkParts[linkParts.length - 1];
+      let data = await processLink(link);
+
+      // modify data.signs to include ID
+      data.signs = data.signs.map((sign) => ({
+        ...sign,
+        categoryId: categoryId,
+      }));
+
+      return {
+        id: categoryId,
+        ...data,
+      };
+    })
+  );
+
+  console.log("processed all links");
+  return processedData;
+};
+
+const processLink = async (link) => {
+  console.log("processing link " + link);
+  const data = await getContent(link);
+  const signCategoryData = getSignCategoryData(data);
+  console.log("processsed link " + link);
+  return signCategoryData;
+};
+
+const getSignCategoryData = (content) => {
+  let doc = silencedDOMParser.parseFromString(content);
+
+  const signNodes = xpath.select(
+    "//div[contains(@class, 'card-link')]",
+    // "//*",
+    doc
+  );
+
+  // get category
+  const signCategoryTitleNode = xpath.select(
+    "//span[contains(@class, 'text-cod-red')]",
+    // "//*",
+    doc
+  );
+
+  const signCategoryTitle = signCategoryTitleNode[0].childNodes[0].nodeValue;
+  // signNode.childNodes
+
+  const signs = signNodes.map((signNode) => {
+    let signData = {};
+    signData.image = signNode
+      .getElementsByTagName("img")[0]
+      .getAttribute("src");
+    signData.title =
+      signNode.getElementsByTagName("h2")[0].childNodes[0].nodeValue;
+
+    try {
+      signData.description =
+        signNode.getElementsByTagName("p")[0].childNodes[0].nodeValue;
+    } catch (error) {
+      signData.description = "";
+    }
+
+    // signData.category = signCategoryTitle;
+    return signData;
+  });
+
+  return {
+    title: signCategoryTitle,
+    signs: signs,
+  };
+};
+
+export const scrapeSigns = async () => {
+  try {
+    const url = "https://www.codrutier.ro/semne-de-circulatie";
+    const indexData = await getContent(url);
+    const signCategoryLinks = getSignCategoryLinks(indexData);
+    let result = await processLinks(signCategoryLinks);
+    console.log(result);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+};
