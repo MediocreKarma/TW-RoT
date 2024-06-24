3548913c332ecf33642a424c989f44f4ca01ebb3
Sabina <citrtoj@gmail.com>
2024-06-24 15:40:20 +0300

    add chapters CRUD

--- back/modules/chapters/index.js
+++ back/modules/chapters/index.js
@@ -3,11 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config({path: '../../.env'});
 
 import {AppRouter} from "../_common/appRouter.js";
-import {getAllChapters, getChapterContent} from "./service.js";
+import { Authentication } from '../_common/appRouter.js';
+import {deleteChapter, getAllChapters, getChapterContent, patchChapter, postChapter} from "./service.js";
 
-const app = new AppRouter('RoT Chapters', process.env.CHAPTERS_PORT);
+const app = new AppRouter('RoT Chapters', process.env.CHAPTERS_PORT, Authentication.REQUIRE);
 
 app.get('/api/v1/chapters', getAllChapters);
+app.post('/api/v1/chapters', postChapter);
 app.get('/api/v1/chapters/:id', getChapterContent);
+app.patch('/api/v1/chapters/:id', patchChapter);
+app.delete('/api/v1/chapters/:id', deleteChapter);
 
 app.start();
--- back/modules/chapters/service.js
+++ back/modules/chapters/service.js
@@ -1,8 +1,9 @@
 import { withDatabaseOperation } from '../_common/db.js';
 import { CSVResponse, ServiceResponse } from '../_common/serviceResponse.js';
 import { ErrorCodes } from '../../common/constants.js';
-import { isStringValidInteger } from '../../common/utils.js';
+import { isStringValidInteger, isAdmin } from '../../common/utils.js';
 import { buildCSVFromPGResult } from '../_common/utils.js';
+import { validateAuth } from "../_common/utils.js";
 
 /**
  * Handler function to get all Traffic Code Chapters
@@ -21,7 +22,7 @@ export const getAllChapters = withDatabaseOperation(async function (
     }
 
     const chapters = (
-        await client.query('select id, number, title, isAddendum from chapter')
+        await client.query('select id, number, title, isaddendum from chapter')
     ).rows;
     return new ServiceResponse(
         200,
@@ -51,7 +52,7 @@ export const getChapterContent = withDatabaseOperation(async function (
 
     const chapter = (
         await client.query(
-            'select number, title, content, isAddendum from chapter where id=$1::int',
+            'select number, title, content, isaddendum from chapter where id=$1::int',
             [id]
         )
     ).rows;
@@ -68,3 +69,286 @@ export const getChapterContent = withDatabaseOperation(async function (
         'Chapter content retrieved successfully'
     );
 });
+
+export const postChapter = withDatabaseOperation(async function(client,
+    _req,
+    _res,
+    params
+) {
+    const chapter = params['body'];
+    
+    const authValidation = validateAuth(params['authorization']);
+    if (authValidation) {
+        return authValidation;
+    }
+    if (!isAdmin(params['authorization'])) {
+        return new ServiceResponse(403, {errorCode: ErrorCodes.UNAUTHORIZED}, 'Unauthorized');
+    }
+    console.log(chapter)
+
+    const title = chapter?.title;
+    const content = chapter?.content;
+    const addendum = chapter?.isaddendum;
+    const number = chapter?.number;
+
+    if (title === undefined) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_TITLE_NOT_IN_BODY },
+            'Chapter title not in body'
+        );
+    }
+
+    if (title === null) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_TITLE },
+            'Chapter title invalid'
+        );
+    }
+
+    if (title?.length > 128) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_TITLE_TOO_LONG },
+            'Chapter title too long'
+        );
+    }
+
+    if (title?.length > 128) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_TITLE_TOO_LONG },
+            'Chapter title too long'
+        );
+    }
+
+    if (content === undefined) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_CONTENT_NOT_IN_BODY },
+            'Chapter content not in body'
+        );
+    }
+    
+    if (content === null) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_CONTENT },
+            'Chapter content invalid'
+        );
+    }
+
+    if (content?.length === 0) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_CONTENT_TOO_SHORT },
+            'Chapter content too short'
+        );
+    }
+
+    if (!(addendum === true || addendum === false)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_ADDENDUM_STATUS },
+            'Invalid chapter addendum status'
+        );
+    }
+        
+    if (number === undefined) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_NUMBER_NOT_IN_BODY },
+            'Chapter number not in body'
+        );
+    }   
+
+    if (!Number.isInteger(number)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_NUMBER },
+            'Invalid chapter number'
+        );
+    }
+
+    const id = (
+        await client.query(
+            'insert into chapter values(default, $1::int, $2::varchar, $3::text, $4::bool) returning id',
+            [chapter.number, chapter.title, chapter.content, chapter.isaddendum],
+        )
+    ).rows[0]['id'];
+
+    const newChapter = (
+        await client.query(
+            'select id, number, title, content, isaddendum from chapter where id=$1::int',
+            [id]
+        )
+    ).rows;
+    if (newChapter.length === 0) {
+        return new ServiceResponse(
+            404,
+            { errorCode: ErrorCodes.CHAPTER_NOT_FOUND },
+            'No chapter with given id'
+        );
+    }
+    return new ServiceResponse(
+        200,
+        newChapter[0],
+        'Chapter added successfully'
+    );
+})
+
+
+export const patchChapter = withDatabaseOperation(async function(client,
+    _req,
+    _res,
+    params
+) {
+    const id = params['path']?.id;
+
+    if (!isStringValidInteger(id)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_ID },
+            'Invalid id format'
+        );
+    }
+    
+    const authValidation = validateAuth(params['authorization']);
+    if (authValidation) {
+        return authValidation;
+    }
+    if (!isAdmin(params['authorization'])) {
+        return new ServiceResponse(403, {errorCode: ErrorCodes.UNAUTHORIZED}, 'Unauthorized');
+    }
+
+    const originalChapter = (
+        await client.query(
+            'select number, title, content, isaddendum from chapter where id=$1::int',
+            [id]
+        )
+    ).rows;
+    if (originalChapter.length === 0) {
+        return new ServiceResponse(
+            404,
+            { errorCode: ErrorCodes.CHAPTER_NOT_FOUND },
+            'No chapter with given id'
+        );
+    }
+
+    const chapter = params['body'];
+
+    const title = chapter?.title;
+    const content = chapter?.content;
+    const addendum = chapter?.isaddendum;
+    const number = chapter?.number;
+
+    if (title !== undefined && title?.length > 128) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_TITLE_TOO_LONG },
+            'Chapter title too long'
+        );
+    }
+    if (title !== undefined && title?.length === 0) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_TITLE_TOO_SHORT },
+            'Chapter title too short'
+        );
+    }
+
+    if (content !== undefined && content?.length === 0) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.CHAPTER_CONTENT_TOO_SHORT },
+            'Chapter content too short'
+        );
+    }
+
+    if (number !== undefined && !Number.isInteger(number)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_NUMBER },
+            'Invalid chapter number'
+        );
+    }
+
+    if (addendum !== undefined && !(addendum === true || addendum === false)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_ADDENDUM_STATUS },
+            'Invalid chapter addendum status'
+        );
+    }
+    
+    if (content === null) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_CONTENT },
+            'Chapter content invalid'
+        );
+    }
+    
+    
+    if (title === null) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_TITLE },
+            'Chapter title invalid'
+        );
+    }
+
+    const newChapter = {
+        ...originalChapter,
+        ...(chapter?.title ? {title: chapter.title} : {}),
+        ...(chapter?.number ? {number: chapter.number} : {}),
+        ...(chapter?.content ? {content: chapter.content} : {}),
+        ...(chapter?.isaddendum ? {isaddendum: chapter.isaddendum} : {}),
+    }
+
+    await client.query(
+        `update chapter
+            set number=$1::int, title=$2::varchar, content=$3::text, isaddendum=$4::bool
+            where id=$5::int`,
+        [newChapter.number, newChapter.title, newChapter.content, newChapter.isaddendum, id],
+    )
+    
+    return new ServiceResponse(
+        204,
+        null,
+        'Chapter patched successfully'
+    );
+})
+
+export const deleteChapter = withDatabaseOperation(async function(client,
+    _req,
+    _res,
+    params
+) {
+    const id = params['path']?.id;
+
+    if (!isStringValidInteger(id)) {
+        return new ServiceResponse(
+            400,
+            { errorCode: ErrorCodes.INVALID_CHAPTER_ID },
+            'Invalid id format'
+        );
+    }
+
+    const updated = (await client.query(
+        `delete from chapter
+            where id=$1::int`,
+        [id])
+    ).rowCount;
+    
+    if (updated === 0) {
+        return new ServiceResponse(404, {errorCode: ErrorCodes.CHAPTER_NOT_FOUND}, `Could not delete nonexistent chapter`);
+    }
+    
+    return new ServiceResponse(
+        204,
+        null,
+        'Chapter deleted successfully'
+    );
+})
